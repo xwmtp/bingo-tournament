2972b7cbbb2075ca3ba80ec90eb72baee13686f9
xwmtp <xwillmtp@gmail.com>
2022-01-30 16:44:16 +0100

    Initialize Spring Boot project

---
+++ src/test/kotlin/com/github/xwmtp/bingotournament/BingoTournamentApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.github.xwmtp.bingotournament
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BingoTournamentApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+}

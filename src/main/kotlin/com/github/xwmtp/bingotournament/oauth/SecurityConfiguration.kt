127398db65e2021999410bc8b27956441f9366b0
scaramangado <scaramangado@gmail.com>
2022-06-05 19:51:37 +0200

    Migrate to SecurityFilterChain

--- src/main/kotlin/com/github/xwmtp/bingotournament/oauth/SecurityConfiguration.kt
+++ src/main/kotlin/com/github/xwmtp/bingotournament/oauth/SecurityConfiguration.kt
@@ -3,10 +3,11 @@ package com.github.xwmtp.bingotournament.oauth
 import com.github.xwmtp.bingotournament.oauth.racetime.RacetimeOauthClient
 import com.github.xwmtp.bingotournament.oauth.racetime.RacetimeUserService
 import com.github.xwmtp.bingotournament.util.FrontendProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 
@@ -15,9 +16,10 @@ class SecurityConfiguration(
 		private val frontendProperties: FrontendProperties,
 		private val racetimeOauthClient: RacetimeOauthClient,
 		private val racetimeUserService: RacetimeUserService,
-) : WebSecurityConfigurerAdapter() {
+) {
 
-	override fun configure(http: HttpSecurity) {
+	@Bean
+	fun configure(http: HttpSecurity): SecurityFilterChain {
 
 		http.exceptionHandling().authenticationEntryPoint(Http403ForbiddenEntryPoint())
 
@@ -34,5 +36,7 @@ class SecurityConfiguration(
 				.antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
 				.antMatchers(HttpMethod.GET, "/api/matches").permitAll()
 				.anyRequest().authenticated()
+
+		return http.build()
 	}
 }

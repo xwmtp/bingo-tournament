86da207600b5d25bfdb386334f6054a6db588b14
scaramangado <scaramangado@gmail.com>
2022-06-12 16:25:52 +0200

    Fix Spring issues

--- src/main/kotlin/com/github/xwmtp/bingotournament/util/GlobalConfiguration.kt
+++ src/main/kotlin/com/github/xwmtp/bingotournament/util/GlobalConfiguration.kt
@@ -2,11 +2,13 @@ package com.github.xwmtp.bingotournament.util
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.web.client.RestTemplate
 
 @Configuration
 class GlobalConfiguration {
 
 	@Bean
+	@Primary
 	fun restTemplate() = RestTemplate()
 }
